92b60b085be5171d60dbedc2bc815073d907adc6
Franz <franz.sotoleal@ing.austral.edu.ar>
2020-02-22 21:12:40 -0300

    generify the vector guide

--- src/tp2/TpVectors.java
+++ src/tp2/TpVectors.java
@@ -1,40 +1,39 @@
 package tp2;
 
-import org.omg.PortableInterceptor.INACTIVE;
 import tp2.utils.Infraction;
 
-public interface TpVectors {
-    int exercise_1_a(int[] a);
+public interface TpVectors<T> {
+    T exercise_1_a(T[] a);
 
-    int exercise_1_b(int[] a);
+    T exercise_1_b(T[] a);
 
-    int exercise_1_c(int[] a, int value);
+    T exercise_1_c(T[] a, T value);
 
-    int exercise_1_d(int[] a);
+    T exercise_1_d(T[] a);
 
 //    TODO: add exercise 1_e
 
-    int exercise_1_f(int[] a);
+    T exercise_1_f(T[] a);
 
-    int[] exercise_1_g(int[] v, int[] w);
+    T[] exercise_1_g(T[] v, T[] w);
 
-    int[] exercise_1_h(int[] v, int[] w);
+    T[] exercise_1_h(T[] v, T[] w);
 
-    int[] exercise_1_i(int[] v, int[] w);
+    T[] exercise_1_i(T[] v, T[] w);
 
-    int[] exercise_1_j(int[] a);
+    T[] exercise_1_j(T[] a);
 
-    int exercise_1_k(int[] v, int[] w);
+    T exercise_1_k(T[] v, T[] w);
 
-    int[] exercise_2(int[] a);
+    T[] exercise_2(T[] a);
 
-    boolean exercise_3(int[] a);
+    boolean exercise_3(T[] a);
 
-    boolean exercise_4(int[] a);
+    boolean exercise_4(T[] a);
 
-    int[] exercise_5_a(Infraction[] a);
+    T[] exercise_5_a(Infraction[] a);
 
-    int[] exercise_5_b(Infraction[] a);
+    T[] exercise_5_b(Infraction[] a);
 
-    int exercise_5_c(Infraction[] a);
+    T exercise_5_c(Infraction[] a);
 }
